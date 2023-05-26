506c3625c6df3536eced77355db40af57e9b5291
Vorongor <mr.maddarknes@gmail.com>
2023-05-26 13:44:40 +0300

    1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,24 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galeryRef = document.querySelector(".gallery");
+
+function makeGalerryMarkap(items) {
+    return items
+    .map((item) => `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+       <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+ </li>`)
+    .join(" ");
+}
+// console.log(makeGalerryMarkap(galleryItems));
+
+galeryRef.innerHTML = makeGalerryMarkap(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a');
