e2863383e5a74cce9b26886f964b393c95469c0e
Vorongor <mr.maddarknes@gmail.com>
2023-05-26 15:45:29 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import { Player } from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+player.on('timeupdate', function(data) {
+  const currentTime = data.seconds;
+  console.log(currentTime);
+});
